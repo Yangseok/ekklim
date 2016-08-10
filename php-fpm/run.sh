3aae782fe4f6ebcbff4773c5b479bf6d8216e18c
Yangseok <ysl8607@naver.com>
2016-08-10 11:18:36 +0900

    Update run.sh

--- php-fpm/run.sh
+++ php-fpm/run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 docker build -t was:0.1 .
-docker run -i -t -v /home/php-fpm/log:/var/log/php-fpm -d -p 9090:9090 --name was was:0.1 /bin/bash
-docker exec was php-fpm
+docker run -v /home/php-fpm/log:/var/log/php-fpm -d -p 9090:9090 --name was was:0.1 php-fpm -F
